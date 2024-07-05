85b6dd663bd578ba9d4692be7eb76ea021e49c43
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-07-04 20:18:11 -0500

    Post mapping is working, tested on Postman. Post is in MySQL

--- src/main/java/com/devryan/employee/service/EmployeeService.java
+++ src/main/java/com/devryan/employee/service/EmployeeService.java
@@ -1,7 +1,18 @@
 package com.devryan.employee.service;
 
+import com.devryan.employee.entity.Employee;
+import com.devryan.employee.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public Employee postEmployee(Employee employee){
+
+        return employeeRepository.save(employee);
+    }
 }
