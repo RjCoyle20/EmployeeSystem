36a4b84ab2f9dc5499833ce52117104d6f50157d
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-07-04 20:11:55 -0500

    backend set up, model, repo and service. Front-end created

---
+++ src/main/java/com/devryan/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.devryan.employee.repository;
+
+import com.devryan.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/main/java/com/devryan/employee/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.devryan.employee.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+}
