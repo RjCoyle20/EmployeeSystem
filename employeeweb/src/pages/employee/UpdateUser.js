883301b2def1e1731e742e75071878e2eea5bff3
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-07-16 21:15:58 -0500

    Finished tutorial! Fully functional. May work on CSS more.

--- employeeweb/src/pages/employee/UpdateUser.js
+++ employeeweb/src/pages/employee/UpdateUser.js
@@ -1,12 +1,13 @@
 import { Button, Form } from 'react-bootstrap';
 import './UpdateUser.css';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 
 
 const UpdateUser = () => {
 
     const {id} = useParams();
+    const navigate = useNavigate();
 
     const [formData, setFormData] = useState ({
         name:"",
@@ -35,12 +36,32 @@ const UpdateUser = () => {
         }
 
         fetchEmployee();
-    }, [id])
+    }, [id]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(`http://localhost:8080/api/employee/${id}`, {
+                method: 'PATCH', 
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+
+            const data = await response.json();
+            console.log("User updated:", data);
+            navigate(`/`)
+        } catch (error) {
+            console.error("Error updating user:", error.message);
+        }
+    }
     return (
         <>
         <div className='center-form'>
             <h1>Edit Employee</h1>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group controlId='formBasicName'>
                     <Form.Control 
                     type='text' 
